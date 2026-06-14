5743f59b307406b342d9b10cd59c4f23cbeddf35
AyushSriva2598 <ayushsriva2598@example.com>
2026-06-14 20:10:36 +0530

    Project Name Change

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HotelManagement.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SphereBnB.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
